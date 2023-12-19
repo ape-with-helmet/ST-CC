47e01379302344a6b77dbd7cccf90904ba9455b5
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-12-19 11:45:14 +0530

    nonoo

---
+++ p2.py
@@ -0,0 +1,9 @@
+#CReate a new branch names "feature-branch". Switch to the master branch and merge the feature branch into the master branch.
+#Stash your changes, switch changes and apply the stahsed changes.
+#commands used: 
+#git branch -m branch_name      THis command renames a branch
+#git checkout branch_name       This command switches to the named branch
+#git branch -v                  This command shows all the branches and the files opened recently
+#git add                        This command adds a file to branch
+#git commit -m "---"            This command commits the changes
+#git push origin from:to        This command is to push from a location to a repo
