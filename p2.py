c84facf8581e33eafdde9f3501e5376f1f54e056
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2024-02-28 03:58:57 +0000

    CommitMessage

--- p2.py
+++
@@ -1,9 +0,0 @@
-#CReate a new branch names "feature-branch". Switch to the master branch and merge the feature branch into the master branch.
-#Stash your changes, switch changes and apply the stahsed changes.
-#commands used: 
-#git branch -m branch_name      THis command renames a branch
-#git checkout branch_name       This command switches to the named branch
-#git branch -v                  This command shows all the branches and the files opened recently
-#git add                        This command stages a file to commit queue
-#git commit -m "---"            This command commits the staged changes
-#git push origin from:to        This command is to push from a location to a repo
